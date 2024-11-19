cb6d682e8d322dff519deea5bd2c732db8014506
emmamarlie <eschstruth.em@gmail.com>
2024-11-19 17:16:14 +0100

    show of item done

--- app/javascript/controllers/price_calculator_controller.js
+++ app/javascript/controllers/price_calculator_controller.js
@@ -3,16 +3,32 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="price-calculator"
 export default class extends Controller {
   static targets = ["startElement", "endElement", "daysElement", "totalElement"]
+  static values = {dailyPrice: Number}
 
   connect() {
     console.log("connecting...")
-    console.log(this.endElementTarget);
-    console.log(this.startElementTarget);
+    // console.log(this.endElementTarget.value);
+    // console.log(this.startElementTarget.value);
   }
 
   calculate() {
-    this.endElementTarget - this.startElementTarget
+    // console.log(this.endElementTarget.value);
+    // console.log(this.startElementTarget.value);
+    if (this.startElementTarget.value && this.endElementTarget.value) {
 
+      const startDateValue = this.startElementTarget.value;
+      const endDateValue = this.endElementTarget.value;
+
+      const startDate = new Date(startDateValue);
+      const endDate = new Date(endDateValue);
+
+      const timeDiff = endDate - startDate;
+      const dayCount = (timeDiff/1000/60/60/24) + 1
+      this.daysElementTarget.innerText = dayCount
+
+      this.totalElementTarget.innerText = dayCount * this.dailyPriceValue + 5
+      console.log(this.dailyPriceValue)
+    }
   }
 }
 
