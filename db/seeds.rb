a5b075b50a4a71321d0f4aeaa3c349b806a3e439
marc-19 <001marc.clotet@gmail.com>
2024-11-22 12:02:11 +0100

    Solved seed + navbar to home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,28 +29,33 @@ end
 
 #Item seed
 items = []
+items_data = [
+  { title: "Casual soft Trousers", category: "Trousers", description: "A stylish and versatile trouser perfect for casual outings.", image: "https://content.asos-media.com/-/media/homepages/mw/2024/september/23-us/mw_asosdesign.jpg" },
+  { title: "Nordstrom Designer vest", category: "Jacket", description: "Elegant and comfortable vest for any occasion.", image: "https://n.nordstrommedia.com/it/889a4d55-8c27-4327-a252-ae877d748e84.jpeg?h=365&w=240&dpr=2" },
+  { title: "Primitive Logo Tee", category: "T-Shirt", description: "Soft cotton tee with a minimalist logo design.", image: "https://image-cdn.hypb.st/https%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F2013%2F10%2FPrimitive_Tee_Blk_2_2.jpg?fit=max&w=460&q=60" },
+  { title: "Tuesday Label Dress", category: "Dress", description: "A light and breezy dress for summer evenings.", image: "https://www.tuesdaylabel.com/cdn/shop/files/TuesdayLabel-SS24-LB-468_1024x1024.jpg?v=1730345552" },
+  { title: "Nude Project Hoodie", category: "Jacket", description: "Cozy and warm hoodie for lounging or going out.", image: "https://nude-project.com/cdn/shop/files/TIMEHOODASH_back.jpg?v=1716972928" },
+  { title: "Levis Slim Fit Jeans", category: "Trousers", description: "Classic denim jeans with a modern slim fit.", image: "https://lsco.scene7.com/is/image/lsco/362000124-detail1-pdp-lse?fmt=jpeg&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=2000&hei=1840" },
+  { title: "Vintage New Balance Shoes", category: "Shoes", description: "Timeless shoes with a vintage vibe. You will love it", image: "https://i.ebayimg.com/images/g/qIgAAOSwtRVmuaBR/s-l1200.jpg" },
+  { title: "Stone Island Puffer Coat", category: "Jacket", description: "High-performance puffer coat for extreme weather.", image: "https://www.zoeteliefmode.nl/wp-content/uploads/2024/07/stone-island-43128-seamless-puffer-coat-bp_15mf_7hk_she55j.jpg" },
+  { title: "Patagonia Fleece Jacket", category: "Jacket", description: "Comfortable and eco-friendly fleece jacket.", image: "https://images.snowleader.com/cdn-cgi/image/f=auto,fit=scale-down,q=85/https://images.snowleader.com/media/catalog/product/cache/1/image/0dc2d03fe217f8c83829496872af24a0/P/A/PATA03589_03.jpg" },
+  { title: "Patagonia Rain Jacket", category: "Jacket", description: "Waterproof and breathable jacket for outdoor adventures.", image: "https://www.switchbacktravel.com/sites/default/files/images/articles/Patagonia%20Boulder%20Fork%20rain%20jacket%20%28backpacking%20in%20Chile%29.jpg" }
+]
 category = ["trousers", "t-shirt", "shoes", "jackets", "dress", "suits"]
 size = ["XS", "S", "M", "L", "XL"]
 condition = ["horrible", "super", "amazing", "mid", "bad", "don't know"]
 
 
-images = ["https://content.asos-media.com/-/media/homepages/mw/2024/september/23-us/mw_asosdesign.jpg",
-          "https://n.nordstrommedia.com/it/889a4d55-8c27-4327-a252-ae877d748e84.jpeg?h=365&w=240&dpr=2",
-        "https://image-cdn.hypb.st/https%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F2013%2F10%2FPrimitive_Tee_Blk_2_2.jpg?fit=max&w=460&q=60",
-      "https://www.tuesdaylabel.com/cdn/shop/files/TuesdayLabel-SS24-LB-468_1024x1024.jpg?v=1730345552",
-      "https://nude-project.com/cdn/shop/files/TIMEHOODASH_back.jpg?v=1716972928",
-      "https://lsco.scene7.com/is/image/lsco/362000124-detail1-pdp-lse?fmt=jpeg&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=2000&hei=1840",
-      "https://i.ebayimg.com/images/g/qIgAAOSwtRVmuaBR/s-l1200.jpg",
-      "https://www.zoeteliefmode.nl/wp-content/uploads/2024/07/stone-island-43128-seamless-puffer-coat-bp_15mf_7hk_she55j.jpg",
-      "https://images.snowleader.com/cdn-cgi/image/f=auto,fit=scale-down,q=85/https://images.snowleader.com/media/catalog/product/cache/1/image/0dc2d03fe217f8c83829496872af24a0/P/A/PATA03589_03.jpg",
-      "https://www.switchbacktravel.com/sites/default/files/images/articles/Patagonia%20Boulder%20Fork%20rain%20jacket%20%28backpacking%20in%20Chile%29.jpg"]
-
+count = 0
 sellers.each do |seller|
-  rand(1..5).times do
-    file = URI.parse(images.sample).open
-    item = Item.create!(user_id: seller.id, category: category.sample, size: size.sample, condition: condition.sample, price_per_day: rand(0..100), description: Faker::GreekPhilosophers.quote, title: Faker::JapaneseMedia::Doraemon.gadget)
-    item.image.attach(io: file, filename: "item.png", content_type: "image/png")
+
+  2.times do
+    file = URI.parse(items_data[count][:image]).open
+    item = Item.create!(user_id: seller.id, size: size.sample, condition: condition.sample, price_per_day: rand(5..40),
+    category: items_data[count][:category], title: items_data[count][:title], description: items_data[count][:description])
+    item.image.attach(io: file, filename: "#{item.title.parameterize}.png", content_type: "image/png")
     items << item
+    count += 1
   end
 end
 
